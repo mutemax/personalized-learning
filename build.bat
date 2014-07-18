7255b96fd103e12bbb0abda46236d023f0bc22db
Andrey Drebot <a.drebot@gmail.com>
2014-07-18 18:32:06 +0300

    #2: Load time of the template settings was improved

--- build.bat
+++ build.bat
@@ -7,7 +7,7 @@ rem clean and copy directory
 echo y| rmdir /s %output%
 
 rem copying everything except of develop-files
-robocopy %cd% %output% /S /XD .idea .git js app css /XF *.sln *.suo *.bat .gitignore build.bat build.js r.js
+robocopy %cd% %output% /S /XD .idea .git js app css /XF *.md *.sln *.suo *.bat .gitignore build.bat build.js r.js
 
 rem copying all files except of css-files: images, fonts etc
 robocopy %cd%\css %output%\css /S /XF *.css
@@ -15,13 +15,20 @@ robocopy %cd%\css %output%\css /S /XF *.css
 rem vendor-libs
 xcopy %cd%\js\vendor.min.js %output%\js\ /I
 xcopy %cd%\js\require\require.js %output%\js\require\ /I
-xcopy %cd%\js\settings\settings.js %output%\js\settings\ /I
 
 rem css minification
 node r.js -o cssIn=css\main.css out=%output%\css\main.css
-node r.js -o cssIn=css\settings.css out=%output%\css\settings.css
+
 
 rem js minification
 node r.js -o build.js out=%output%\app\main.js
+
+rem settings
+
+xcopy %cd%\settings\js\vendor.min.js %output%\settings\js\ /I
+xcopy %cd%\settings\js\settings.js %output%\settings\js\ /I
+robocopy %cd%\settings\css\fonts %output%\settings\css\fonts
+robocopy %cd%\settings\img %output%\settings\img
+node r.js -o cssIn=settings\css\settings.css out=%output%\settings\css\settings.css
 
 echo Build output folder: %output%
