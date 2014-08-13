2561ba6f57d9eb6b0dcfd72e809a3e6cd35186a4
Andrey Drebot <a.drebot@gmail.com>
2014-08-13 10:39:29 +0300

    #16: Fill in the blanks dropdowns

--- app/bootstrapper.js
+++ app/bootstrapper.js
@@ -1,23 +1,28 @@
-﻿define(['customBindings', 'durandal/system',  'plugins/http'], function(customBindings, system, http) {
+﻿define(function (require) {
     "use strict";
 
+    var customBindings = require('customBindings');
     customBindings.init();
 
-    system.defer = function(action) {
+
+    var system = require('durandal/system');
+    system.defer = function (action) {
         var deferred = Q.defer();
         action.call(deferred, deferred);
         var promise = deferred.promise;
-        deferred.promise = function() {
+        deferred.promise = function () {
             return promise;
         };
         return deferred;
     };
 
-    http.get = function(url, query) {
+
+    var http = require('plugins/http');
+    http.get = function (url, query) {
         return Q($.ajax(url, { data: query }));
     };
 
-    http.post = function(url, data) {
+    http.post = function (url, data) {
         return Q($.ajax({
             url: url,
             data: ko.toJSON(data),
@@ -26,6 +31,35 @@
             dataType: 'json'
         }));
     };
+
+    var binder = require('durandal/binder');
+    binder.bindingComplete = (function () {
+
+        var handlers = {};
+
+        var f = function (data, view) {
+            var key = $(view).attr('data-view');
+
+            if (key && handlers[key]) {
+                _.each(handlers[key], function (handler) {
+                    handler.apply(null, [view]);
+                });
+            }
+        }
+
+        f.addHandler = function (key, handler) {
+            if (!_.isArray(handlers[key])) {
+                handlers[key] = [];
+            }
+
+            handlers[key].push(handler);
+        }
+
+        return f;
+
+    })();
+    binder.bindingComplete.addHandler('views/FillInTheBlanks', require('views/handlers/FillInTheBlanks'));
+
 
     return {
 
