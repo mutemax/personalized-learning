c5dc3040c00ff91de3a92ba149725b20e678d655
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2014-12-04 18:12:09 +0200

    Change gulpfile: remove versioning for settings.js file.

--- gulpfile.js
+++ gulpfile.js
@@ -86,7 +86,6 @@ gulp.task('build-settings', ['clean'], function () {
       .pipe(gulp.dest(output + '/settings/img'));
 
     return gulp.src(['settings/js/settings.js'])
-      .pipe(addBuildVersion())
       .pipe(uglify())
       .pipe(gulp.dest(output + '/settings/js'));
 });
