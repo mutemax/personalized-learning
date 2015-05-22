6bb10e54b6cedfb8986d6e8d4053cdbe445ba7f4
LeonidPelyhivskyi <plo.91ua@gmail.com>
2015-05-22 09:47:30 +0300

    Add support hotspot on image to PL

--- app/_components/bindingHandlers/learningContentBindingHandler.js
+++ app/_components/bindingHandlers/learningContentBindingHandler.js
@@ -1,34 +1,36 @@
-define(['durandal/composition', 'knockout'], function (composition, ko) {
+define(['durandal/composition'], function (composition) {
+    'use strict';
     
     return {
         install: install
     };
     
-    function install(params) {
+    function install(){
         ko.bindingHandlers.learningContent = {
             init: function (element, valueAccessor) {
                 var $element = $(element),
                     html = valueAccessor();
-                
-                var $output = $('<output>');
-                $output.html(html);
-                
-                var learningContent = $('[data-type]', $output);
-                var dataType = learningContent.attr('data-type');
-                
-                
+
+                var dataType = getLearningContentType(html);
+
                 switch(dataType){
                     case 'hotspot': 
                         var hotspotOnImage = new HotspotOnImage($(html)[0]);
                         $element.html(hotspotOnImage.element);
+                        break;
                     default:
                         $element.html(html);
                 }
             }
         };
+
+        composition.addBindingHandler('learningContent');
+
+        function getLearningContentType(data){
+            var $output = $('<output>').html(data),
+                dataType = $('[data-type]', $output).attr('data-type');
+
+            return dataType;
+        }
     }
-    
-    
-    
-    composition.addBindingHandler('learningContent');
 });
