e5b1089b8c2c9fef86bd999ea10e310573571058
LeonidPelyhivskyi <plo.91ua@gmail.com>
2015-10-28 09:00:31 +0200

    Incorrect Branchtrack question score returned in case of incorrect answer.

--- app/entities/ScenarioQuestion.js
+++ app/entities/ScenarioQuestion.js
@@ -17,7 +17,7 @@
         that.masteryScore = masteryScore;
 
         function answer(score) {
-            that.score = score >= that.masteryScore ? 100 : score;
+            that.score = score >= that.masteryScore ? 100 : 0;
         }
 
         function restoreProgress(progress) {
